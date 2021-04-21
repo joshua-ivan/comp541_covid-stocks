fd08ae588b70c251674234cfc677abb6f5a9c46e
JoshL <joshua.licudo.753@my.csun.edu>
2021-04-21 16:21:31 -0700

    Allow exchange directories with spaces

--- percent_performance.sh
+++ percent_performance.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 declare -a exchanges=("AMEX" "NASDAQ" "NYSE" "NYSE Arca" "OTC")
-for exchange in $exchanges ; do
+for exchange in "${exchanges[@]}" ; do
     for letter_one in {a..z} ; do
         for letter_two in {a..z} ; do
             python3 percent_performance.py $exchange $letter_one$letter_two
